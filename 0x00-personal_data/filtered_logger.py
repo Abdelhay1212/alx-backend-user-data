8f2d2bd856a84c49837a8cdcf2ed13235106b0fe
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-02-08 12:17:59 +0000

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 ''' Regex-ing '''
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
+    ''' filter datum '''
     for field in fields:
-        pattern = f'(?<={field}=)[^;]+'
-        message = re.sub(pattern, redaction, message)
+        message = re.sub(f'(?<={field}=)[^;]+', redaction, message)
+    message = message.replace(';', separator)
     return message
