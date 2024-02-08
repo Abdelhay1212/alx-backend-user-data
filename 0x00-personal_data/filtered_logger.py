935abc57fb6527e6eeefa81aeabad0e14237d0e3
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-02-08 12:06:19 +0000

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,6 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    ''' filter datum '''
     for field in fields:
         pattern = f'(?<={field}=)[^;]+'
         message = re.sub(pattern, redaction, message)
