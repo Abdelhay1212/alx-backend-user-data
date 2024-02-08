f8a8b823ab4a8de9cd4267acb3b4f71b6ba41acd
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-02-08 16:09:43 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 ''' Regex-ing '''
 import re
+import logging
 from typing import List
 
 
@@ -10,3 +11,24 @@ def filter_datum(
     ''' filter datum '''
     pattern = f"({'|'.join(fields)})=([^{separator}]+)"
     return re.sub(pattern, r"\1=" + redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_format = super().format(record)
+        return filter_datum(self.fields,
+                            RedactingFormatter.REDACTION,
+                            original_format,
+                            RedactingFormatter.SEPARATOR
+                            )
