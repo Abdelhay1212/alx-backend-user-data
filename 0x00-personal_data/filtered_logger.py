2c62f5dc0607f6ce0c8232e951cc4862b5714189
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-02-08 16:55:48 +0000

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,7 +24,7 @@ def get_logger() -> logging.Logger:
 
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
-    formatter = RedactingFormatter(PII_FIELDS)
+    formatter = RedactingFormatter(list(PII_FIELDS))
     stream_handler.setFormatter(formatter)
 
     logger.addHandler(stream_handler)
