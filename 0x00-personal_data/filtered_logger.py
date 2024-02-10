2d46f0ee00a6350d7e9258af048a5480ac9d0b72
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-02-10 15:32:06 +0000

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,24 +34,6 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Creates a connector to a database.
-    """
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-
-    connection = mysql.connector.connect(
-        host=db_host,
-        port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
-    )
-    return connection
-
-
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -68,3 +50,37 @@ class RedactingFormatter(logging.Formatter):
         ''' formats a LogRecord. '''
         msg = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    ''' returns a connector to the database '''
+    config = {
+        'port': 3306,
+        'host': os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
+        'user': os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root'),
+        'passwd': os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''),
+        'database': os.environ.get('PERSONAL_DATA_DB_NAME', '')
+    }
+    try:
+        connection = mysql.connector.connect(**config)
+        return connection
+    except Error as e:
+        print(e)
+        return None
+
+
+def main() -> None:
+    ''' Read and filter data '''
+
+    query = "SELECT name, email, phone, ssn, password, ip, last_login, user_agent FROM users;"
+
+    logger = get_logger()
+    connection = get_db()
+
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+
+if __name__ == '__main__':
+    main()
