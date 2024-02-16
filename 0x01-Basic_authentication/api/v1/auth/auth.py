53d4026390be880d3454a5b43b675f2809ec3b81
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-02-16 19:45:21 +0100

    created the auth folder and the class Auth that represent the template for all authentication system

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+""" Auth class """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user
+        """
+        return None
