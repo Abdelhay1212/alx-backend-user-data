1d4c6aaa46968490277df59685ef796caf698e01
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-02-17 00:50:24 +0100

    fixed pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,8 @@ class BasicAuth(Auth):
     """basic auth
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """extract base64 authorization header
         """
         if authorization_header is None or type(authorization_header) != str \
