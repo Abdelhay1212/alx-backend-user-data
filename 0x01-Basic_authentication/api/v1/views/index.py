df00e31a50d833cc849d493542524ef2078c0234
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-02-16 19:20:19 +0100

    updated

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,8 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
-def raise_unauthorized() -> None:
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
     """Error handler: Unauthorized
     """
     abort(401)
