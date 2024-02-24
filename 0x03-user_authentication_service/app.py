b851bd8b548a8cea5defac6b70aef338b0d1656b
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-02-24 21:03:37 +0100

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,10 +53,10 @@ def handle_sessions():
     if request.method == 'DELETE':
         session_id = request.cookies.get('session_id')
         user = AUTH.get_user_from_session_id(session_id)
-        if user is not None:
-            AUTH.destroy_session(user.id)
-            return redirect('/')
-        abort(403)
+        if user is None:
+            abort(403)
+        AUTH.destroy_session(user.id)
+        return redirect('/')
 
 
 if __name__ == "__main__":
