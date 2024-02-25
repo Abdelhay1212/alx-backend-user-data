e7e31acf2e3707ee8355ea4e76af98eb16051616
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-02-25 02:54:38 +0100

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,5 +63,20 @@ def handle_sessions():
         return redirect('/')
 
 
+@app.route('/profile', strict_slashes=False)
+def profile():
+    """user profile
+    """
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
